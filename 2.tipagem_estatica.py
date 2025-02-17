c8cc8be03bcf094c9e6cb370ec51ef41fc7a26bf
ehos08 <enzosanttos2407@gmail.com>
2025-02-17 18:43:13 +0000

    loading...

---
+++ 2.tipagem_estatica.py
@@ -0,0 +1,16 @@
+import os
+os.system("clear")
+
+#definição de tipos obrigatória
+
+nome: str # mesmo que "caracter"
+idade: int # mesmo que "inteiro"
+altura: float# mesmo que "real"
+  
+
+nome= "beatriz"
+idade= 16
+altura= 1.63
+print(nome)
+print(idade)
+print(altura)
