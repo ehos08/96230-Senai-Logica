98f343a3217da0e5a922e1a2082bd0ec2e704cce
ehos08 <enzosanttos2407@gmail.com>
2025-04-23 19:52:43 +0000

    array 23/04/25

--- 6.listas/7.exercicioNegativos.py
+++ 6.listas/7.exercicioNegativos.py
@@ -3,10 +3,11 @@ os.system("cls||clear")
 
 list = []
 QUANTIDADE = 5
-for i in range(QUANTIDADE):
+def vetor():
+ for i in range(QUANTIDADE):
     numero = int(input(f"Digite o {i+1}º número: "))
     if numero < 0:
-        numero = 0  
+        numero = 0
     list.append(numero)
-
+vetor()
 print("Valores na lista:", list)
